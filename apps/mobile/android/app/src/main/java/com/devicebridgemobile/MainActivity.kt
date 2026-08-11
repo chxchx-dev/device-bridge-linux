793f853c72f6d19b05d612f9446bb194d9762ce5
chxchx-dev <christgg1997@gmail.com>
2026-08-11 12:24:09 -0500

    feat(android): enable immersive cockpit display

--- apps/mobile/android/app/src/main/java/com/devicebridgemobile/MainActivity.kt
+++ apps/mobile/android/app/src/main/java/com/devicebridgemobile/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.devicebridgemobile
 
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    hideSystemBars()
+  }
+
+  override fun onResume() {
+    super.onResume()
+    hideSystemBars()
+  }
+
+  private fun hideSystemBars() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      window.insetsController?.let { controller ->
+        controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+        controller.systemBarsBehavior =
+          android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+      }
+    } else {
+      @Suppress("DEPRECATION")
+      window.decorView.systemUiVisibility = (
+        View.SYSTEM_UI_FLAG_FULLSCREEN or
+          View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+          View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+          View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+          View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+          View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        )
+    }
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
